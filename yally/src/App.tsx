48e988a3401b45d886a3887f15049d082a9b75fd
CHAEEUN-LEE <lce4376@gmail.com>
2019-11-12 22:38:20 +0900

    Set webpack

---
+++ yally/src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Switch, BrowserRouter, Route } from "react-router-dom";
+
+const App: React.FC = () => <div>App.tsx</div>;
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById("app"));
